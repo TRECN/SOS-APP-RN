59862a597433a887439566da815756a9c85b61d4
TRECN <rishabh1103@outlook.com>
2023-02-15 20:34:46 +0530

    adding edit and del buttons

--- SOS_App/component/Contacts.js
+++ SOS_App/component/Contacts.js
@@ -17,7 +17,23 @@ export default function Contacts() {
     }
   ]
 
+  const Edit=()=>(
+    <TouchableOpacity>
+      <View>
+        <Text>EDIT</Text>
+      </View>
+    </TouchableOpacity>
+  )
 
+  const Del=()=>(
+    <TouchableOpacity onPress={()=>{
+      Alert.alert('data deleted')
+    }}>
+      <View>
+        <Text>DELETE</Text>
+      </View>
+    </TouchableOpacity>
+  )
 
   return (
     <View style={{display:'flex',justifyContent:'center',alignItems:'center',height:'100%',borderWidth:10}}>
